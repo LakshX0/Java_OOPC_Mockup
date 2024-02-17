c8191b768fb2a42053e28e0a84c93a31e0d2c835
LakshX0 <lakshansandeep7@gmail.com>
2024-02-17 07:23:27 +0530

    Enapsulated(Getters and Setters)

--- src/com/lakshx/Employee.java
+++ src/com/lakshx/Employee.java
@@ -1,10 +1,30 @@
 package com.lakshx;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
-        return baseSalary + (extraHours * hourlyRate);
+
+        return baseSalary + (extraHours * getHourlyRate());
+    }
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Base Salary Cannot be Zero or Less!");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 10)
+            throw new IllegalArgumentException("Hourly rate cannot be 10 or Less!");
+        this.hourlyRate = hourlyRate;
     }
 }
--- src/com/lakshx/Main.java
+++ src/com/lakshx/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(50_000);
+        employee.getBaseSalary();
+        employee.setHourlyRate(20);
+        employee.getHourlyRate();
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
