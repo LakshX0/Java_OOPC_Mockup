c3da7c759c0100c8b54ba8d4f51195371b78e9bc
LakshX0 <lakshansandeep7@gmail.com>
2024-02-17 06:44:23 +0530

    minnor changes

--- src/com/lakshx/Main.java
+++ src/com/lakshx/Main.java
@@ -3,6 +3,20 @@ package com.lakshx;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 20;
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
     }
-}
+    }
+
+
+//	    var textBox1 = new TextBox();
+//        textBox1.setText("Box 1");
+//        System.out.println(textBox1.text.toUpperCase());
+//
+//        var textBox2 = new TextBox();
+//        textBox2.setText("Box 2");
+//        System.out.println(textBox2.text);
+
